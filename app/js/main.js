fb78825ba9587f88035aa2d695409a1f38812cd9
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-08-22 23:09:40 +0300

    added info and categories

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,8 @@
 $(function(){
     $('.top-slider__inner').slick({
         dots: true,
-        aarrows: false,
+        arrows: false,
         fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000
+        autoplay: true
     });
 }); 
