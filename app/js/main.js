69672be3af04ed7c507d15cdfc0ec6d3d7e9ef7c
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-08-24 23:11:42 +0300

    added products section

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,11 @@ $(function(){
         fade: true,
         autoplay: true
     });
+
+    $(".star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+      });
 }); 
--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
