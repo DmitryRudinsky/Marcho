41fe51c353056bc96e85a73b963b795f8c201a3b
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-08-25 03:37:52 +0300

    added promo and partners section

--- app/js/main.js
+++ app/js/main.js
@@ -12,4 +12,49 @@ $(function(){
         ratedFill: "#ffc35b",
         readOnly: true
       });
+
+    
+    
+      function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24));
+        
+        return {
+          total,
+          days,
+          hours,
+          minutes,
+          seconds
+        };
+      }
+      
+      function initializeClock(id, endtime) {
+        const clock = document.querySelector('.promo__clock');
+        const daysSpan = clock.querySelector('.promo__days');
+        const hoursSpan = clock.querySelector('.promo__hours');
+        const minutesSpan = clock.querySelector('.promo__minutes');
+        const secondsSpan = clock.querySelector('.promo__seconds');
+      
+        function updateClock() {
+          const t = getTimeRemaining(endtime);
+      
+          daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+      
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+      }
+      
+      const deadline = $('.promo__clock').attr('data-time');
+      initializeClock('promo__clock', deadline);
 }); 
