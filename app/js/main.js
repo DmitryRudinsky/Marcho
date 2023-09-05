0b315dde87fc5792fe9e620f4d9a8b2f83e9ac45
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-09-05 18:59:22 +0300

    shop was fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,5 @@
 $(function(){
 
-
-    $('.product-tabs__top-item').on('click', function(e) {
-      e.preventDefault();
-      $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-      $(this).addClass('product-tabs__top-item--active');
-      
-      $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-      $($(this).attr('href')).addClass('product-tabs__content-item--active');
-    });
-
-    $('.product-slide__thumb').slick({
-      asNavFor: '.product-slide__big',
-      focusOnSelect: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      vertical: true,
-      draggable: false                                  
-    });
-
-    $('.product-slide__big').slick({
-      asNavFor: '.product-slide__thumb',
-      draggable: false,
-      arrows: false,
-      fade: true
-    });
-
-
     $('.shop-content__filter-btn').on('click', function() {
       $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
       $(this).addClass('shop-content__filter-btn--active')
@@ -41,7 +14,7 @@ $(function(){
       $('.products-item').removeClass('products-item--list')
     });
 
-    $('.select-style, .product-item__num').styler();
+    $('.select-style').styler();
 
 
     $('.filter-price__input').ionRangeSlider({
