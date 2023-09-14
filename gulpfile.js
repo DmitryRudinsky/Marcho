60d3caf241b24559f45b9749596762d5e19afce1
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-09-15 01:15:31 +0300

    index moduling added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const nunjucksRender = require('gulp-nunjucks-render');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 const browserSync = require('browser-sync').create();
@@ -17,10 +19,20 @@ function browsersync() {
     })
 }
 
+function nunjucks(){
+    return src('app/*.njk')
+        .pipe(nunjucksRender())
+        .pipe(dest('app'))
+        .pipe(browserSync.stream()) 
+}
+
 function styles() {
-    return src('app/scss/style.scss')
+    return src('app/scss/*.scss')
     .pipe(scss({outputStyle: 'compressed'}))
-    .pipe(concat('style.min.css'))
+    // .pipe(concat())
+    .pipe(rename({
+        suffix : '.min'
+    }))
     .pipe(autoprefixer({
         overrideBrowserslist: ['last  10 versions'],
         grid: true
@@ -76,6 +88,7 @@ function cleanDist(){
 
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
+    watch(['app/*.njk'], nunjucks);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
     watch(['app/**/*.html']).on('change', browserSync.reload);
 }
@@ -85,6 +98,7 @@ exports.scripts = scripts
 exports.browsersync = browsersync
 exports.watching = watching;
 exports.images = images;
+exports.nunjucks = nunjucks;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(nunjucks, styles, scripts, browsersync, watching);
