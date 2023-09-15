4fea66b27c0cecc5b268ada704cb9cd53a722c8d
DmitryRudinsky <guladmitriy1331@gmail.com>
2023-09-15 12:19:57 +0300

    added addaptive

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ function cleanDist(){
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/**/*.scss'], styles);
     watch(['app/*.njk'], nunjucks);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
     watch(['app/**/*.html']).on('change', browserSync.reload);
